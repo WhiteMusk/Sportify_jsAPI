b20e0836d0bed4690deb5cf473cc2ad401a2146b
JJJamieee <b07705033@ntu.edu.tw>
2021-05-05 02:09:31 +0800

    fix navbar bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,79 +24,79 @@ function Navbar() {
 
     return (
         <AppBar position="fixed" style={{ zIndex: 1201 }}>
-            {!isTabletOrMobile?
-            <Toolbar>
-                <Button color="inherit" component={Link} to="/">
-                    <FormattedMessage id="app.title" />
-                </Button>
-                {/* Show ManageEvents link if user is logged in, else show Services link */
-                    userData ?
-                        <Button color="inherit" component={Link} to={`/manage/${userData.profile._id}/all`}>
-                            <FormattedMessage id="navbar.manageEvents" />
-                        </Button> :
-                        <Button color="inherit" component={Link} to="/about">
-                            <FormattedMessage id="navbar.about" />
-                        </Button>
-                }
-                <div style={{ flexGrow: 1 }}></div>
-                <Button color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.zh))}>中文</Button>
-                <Button color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.en))}>ENGLISH</Button>
-                {/* Show log out button if user is currently logged in, and vice versa */
-                    userData ?
-                        <>
-                            <Button color="inherit" component={Link} to={`/manage/${userData.profile._id}/organizerInfo`}>
-                                <FormattedMessage id="navbar.hostInfo" />
+            {!isTabletOrMobile ?
+                <Toolbar>
+                    <Button color="inherit" component={Link} to="/">
+                        <FormattedMessage id="app.title" />
+                    </Button>
+                    {/* Show ManageEvents link if user is logged in, else show Services link */
+                        userData ?
+                            <Button color="inherit" component={Link} to={`/manage/${userData.profile._id}/all`}>
+                                <FormattedMessage id="navbar.manageEvents" />
+                            </Button> :
+                            <Button color="inherit" component={Link} to="/about">
+                                <FormattedMessage id="navbar.about" />
                             </Button>
-                            <Typography>
-                                {userData.profile.name}
-                            </Typography>
-                            <Button color="inherit" component={Link} to="/" onClick={() => dispatch(logOut())}>
-                                <FormattedMessage id="logout" />
+                    }
+                    <div style={{ flexGrow: 1 }}></div>
+                    <Button color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.zh))}>中文</Button>
+                    <Button color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.en))}>ENGLISH</Button>
+                    {/* Show log out button if user is currently logged in, and vice versa */
+                        userData ?
+                            <>
+                                <Button color="inherit" component={Link} to={`/manage/${userData.profile._id}/organizerInfo`}>
+                                    <FormattedMessage id="navbar.hostInfo" />
+                                </Button>
+                                <Typography>
+                                    {userData.profile.name}
+                                </Typography>
+                                <Button color="inherit" component={Link} to="/" onClick={() => dispatch(logOut())}>
+                                    <FormattedMessage id="logout" />
+                                </Button>
+                            </>
+                            :
+                            <Button color="inherit" component={Link} to="/auth">
+                                {/* <Button color="inherit" component={Link} to="/manage/60719d96ccb9eb6520edba71/all" onClick={() => dispatch(logIn())}> */}
+                                <FormattedMessage id="login" />
                             </Button>
-                        </>
-                        :
-                        <Button color="inherit" component={Link} to="/auth">
-                        {/* <Button color="inherit" component={Link} to="/manage/60719d96ccb9eb6520edba71/all" onClick={() => dispatch(logIn())}> */}
-                            <FormattedMessage id="login" />
-                        </Button>
-                
-                }
-                </Toolbar>:
+
+                    }
+                </Toolbar> :
                 <Toolbar>
 
-                <IconButton color="inherit" component={Link} to="/">
-                <SportsTennisIcon/>
-                </IconButton>
-                {/* Show ManageEvents link if user is logged in, else show Services link */
-                    isLoggedIn ?
-                        <IconButton color="inherit" component={Link} to="/manage/60719d96ccb9eb6520edba71/all">
-                           <DashboardIcon/>
-                        </IconButton> :
-                        <IconButton color="inherit" component={Link} to="/about">
-                           <InfoIcon/>
-                        </IconButton>
-                }
-                <div style={{ flexGrow: 1 }}></div>
-                <IconButton size ='small' color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.zh))}>CH</IconButton>
-                <IconButton size='small' color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.en))}>EN</IconButton>
-                {/* Show log out button if user is currently logged in, and vice versa */
-                    isLoggedIn ?
-                        <>
-                            <IconButton color="inherit" component={Link} to="/manage/60719d96ccb9eb6520edba71/organizerInfo">
-                            <ContactMailIcon/>
+                    <IconButton color="inherit" component={Link} to="/">
+                        <SportsTennisIcon />
+                    </IconButton>
+                    {/* Show ManageEvents link if user is logged in, else show Services link */
+                        userData ?
+                            <IconButton color="inherit" component={Link} to={`/manage/${userData.profile._id}/all`}>
+                                <DashboardIcon />
+                            </IconButton> :
+                            <IconButton color="inherit" component={Link} to="/about">
+                                <InfoIcon />
                             </IconButton>
-                            <IconButton size='small'  color="inherit" component={Link} to="/" onClick={() => dispatch(logOut())}>
-                            <ExitToAppIcon/>
+                    }
+                    <div style={{ flexGrow: 1 }}></div>
+                    <IconButton size='small' color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.zh))}>CH</IconButton>
+                    <IconButton size='small' color="inherit" onClick={() => dispatch(setLocale(LOCALE_OPTIONS.en))}>EN</IconButton>
+                    {/* Show log out button if user is currently logged in, and vice versa */
+                        userData ?
+                            <>
+                                <IconButton color="inherit" component={Link} to={`/manage/${userData.profile._id}/organizerInfo`}>
+                                    <ContactMailIcon />
+                                </IconButton>
+                                <IconButton size='small' color="inherit" component={Link} to="/" onClick={() => dispatch(logOut())}>
+                                    <ExitToAppIcon />
+                                </IconButton>
+                            </>
+                            :
+                            <IconButton size='small' color="inherit" component={Link} to="/auth">
+                                <FormattedMessage id="login" />
                             </IconButton>
-                        </>
-                        :
-                        <IconButton size='small' color="inherit" component={Link} to="/manage/60719d96ccb9eb6520edba71/all" onClick={() => dispatch(logIn())}>
-                            <FormattedMessage id="login" />
-                        </IconButton>
-                }
-            
-            </Toolbar>
-}
+                    }
+
+                </Toolbar>
+            }
         </AppBar>
     );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -19,10 +19,10 @@ const useStyles = makeStyles((theme) => ({
         paddingBottom: theme.spacing(4)
     },
     appBarSpacer: theme.mixins.toolbar,
-    divider:{
+    divider: {
         marginTop: theme.spacing(4),
     },
-    divider2:{
+    divider2: {
         marginTop: theme.spacing(4),
         marginBottom: theme.spacing(4),
     }
@@ -30,108 +30,105 @@ const useStyles = makeStyles((theme) => ({
 
 function HomePage() {
     const classes = useStyles();
-    const { loading, error, data } = useQuery
-    (GET_EVENTS_QUERY);
+    const { loading, error, data, refetch } = useQuery
+        (GET_EVENTS_QUERY);
     const [eventList, setEventList] = useState([]);
-    const [searchField,setSearchField] = useState('');
-    const [Region,setRegion] = useState('');
-    const [Level,setLevel] = useState('');
-    const [date,setDate] = useState([])
+    const [searchField, setSearchField] = useState('');
+    const [Region, setRegion] = useState('');
+    const [Level, setLevel] = useState('');
+    const [date, setDate] = useState([])
     if (error) console.log(error);
-    if (!eventList.length && data)
-    {
-    setEventList(data.getEvents);
+    if (!eventList.length && data) {
+        setEventList(data.getEvents);
     }
     const onSearchChange = (event) => {
         setSearchField(event.target.value)
-      }
-    const onRegionChange = (event) =>{
+    }
+    const onRegionChange = (event) => {
         setRegion(event.target.value)
     }
-    const onLevelChange = (event) =>{
+    const onLevelChange = (event) => {
         setLevel(event.target.value)
     }
-    const onDateChange = (value) =>{
+    const onDateChange = (value) => {
         setDate(value)
     }
-    const searchEventList1 = eventList.filter(event=>{
-        if (!date || !date.length){return true}
-        else if (!event.date){return false}
-        else
-        {  
-           var time = event.date; 
-           if(event.date.length>10)
-           {
-            var time = moment(new Date(parseInt(event.date))).format('YYYY-MM-DD');
-           }
-           console.log(time);
-           if(date[0].isAfter(time) || date[1].isBefore(time)){
-               return false
-           }
-           else{
-               return true}
+    const searchEventList1 = eventList.filter(event => {
+        if (!date || !date.length) { return true }
+        else if (!event.date) { return false }
+        else {
+            var time = event.date;
+            if (event.date.length > 10) {
+                var time = moment(new Date(parseInt(event.date))).format('YYYY-MM-DD');
+            }
+            console.log(time);
+            if (date[0].isAfter(time) || date[1].isBefore(time)) {
+                return false
+            }
+            else {
+                return true
+            }
         }
     })
-  
-   
-    const searchEventList = searchEventList1.length? searchEventList1.filter(event=>{
-        if(!event.region||!event.level)
-        {
+
+
+    const searchEventList = searchEventList1.length ? searchEventList1.filter(event => {
+        if (!event.region || !event.level) {
             return false;
         }
-        else{
-            return (event.title.includes(searchField) || 
-            event.location.includes(searchField)) && 
-            (event.region.includes(Region) &&
-            event.level.includes(Level))
+        else {
+            return (event.title.includes(searchField) ||
+                event.location.includes(searchField)) &&
+                (event.region.includes(Region) &&
+                    event.level.includes(Level))
         }
-        }):searchEventList1
-    
+    }) : searchEventList1
+
     useEffect(() => {
         refetch();
     }, [data]);
 
     return (
         <>
-        <NavbarH searchChange={onSearchChange} />
-        <Container maxWidth="md" className={classes.container}>
-            <div className={classes.appBarSpacer} />
-            {data ?
-            <>
-            <Bulletin eventList={data.getEvents}/> 
-            <Divider variant='middle' className={classes.divider}/>
-            
-            <Filter 
-            value = {date}
-            DateChange={onDateChange}
-            RegionChange={onRegionChange} 
-            LevelChange={onLevelChange}/>
-                <Divider variant='middle' className={classes.divider2}/>
-            <Typography gutterBottom variant="h5" component="h2">
-                <FormattedMessage id="homePage.allEvents" />
-            </Typography>
-            </>
-            : <></>
-            }
-            
-            {loading ?
-                <Typography>
-                    <FormattedMessage id="loading" />
-                </Typography> : (
-                    data ?(searchEventList.length?
-                        <EventCardList events={searchEventList} />:
-                        <Typography>
-                            <FormattedMessage id="No Result"/>
-                    </Typography>
+            <NavbarH searchChange={onSearchChange} />
+            <Container maxWidth="md" className={classes.container}>
+                <div className={classes.appBarSpacer} />
+                {data ?
+                    <>
+                        <Bulletin eventList={data.getEvents} />
+                        <Divider variant='middle' className={classes.divider} />
+
+                        <Filter
+                            value={date}
+                            DateChange={onDateChange}
+                            RegionChange={onRegionChange}
+                            LevelChange={onLevelChange} />
+                        <Divider variant='middle' className={classes.divider2} />
+                        <Typography gutterBottom variant="h5" component="h2">
+                            <FormattedMessage id="homePage.allEvents" />
+                        </Typography>
+                    </>
+                    : <></>
+                }
+
+                {loading ?
+                    <Typography>
+                        <FormattedMessage id="loading" />
+                    </Typography> : (
+                        data ? (searchEventList.length ?
+                            <EventCardList events={searchEventList} /> :
+                            <Typography>
+                                <FormattedMessage id="No Result" />
+                            </Typography>
                         )
-                        :
-                        <Typography>
-                            Cannot connect to server.
+                            :
+                            <Typography>
+                                Cannot connect to server.
                     </Typography>)
-            }
-        </Container>
-        <Divider/>
-            <Footer/>
+                }
+            </Container>
+            <Divider />
+            <Footer />
         </>
     )
 }
